20c6ba1aea2ba9d1b1657b5d515b916ed3a2997c
Daniel Osorio Merchan <didanito@gmail.com>
2015-04-04 20:01:12 -0500

    filtro de eventos

--- wp-content/themes/manawynwood/page-events-list.php
+++ wp-content/themes/manawynwood/page-events-list.php
@@ -53,13 +53,21 @@
 			    	</div>
 				<?php endwhile; endif; wp_reset_query(); ?> 
 			</div>
+			
 
 			<div class="large-12 columns full-posts">
+				<ul id='filter' class="filter large-12 columns">
+					<li class='large-3 columns'><a class="curren-event" href='#'>current</a></li>
+					<li class='large-3 columns'><a class="upcomming-event" href='#'>up comming</a></li>
+					<li class='large-3 columns'><a class="past-event" href='#'>past</a></li>
+					<li class='large-3 columns current'><a class="all-events" href='#'>all Events</a></li>
+				</ul>
+			
 				<?php $query = query_posts( 'post_type=sc_event&offset=3' ); if (have_posts() ) : while(have_posts()) : the_post(); 
 					$category = get_the_category();
 					$cat_name = ($category[0]->cat_name);
 				?>
-		    	<div class="item large-3 columns <?php echo $cat_name ?>">
+		    	<div class="item animated large-3 columns <?php echo $cat_name ?>">
 			  		<div class="content-item">
 				  		<div class="large-12 columns container-image">
 				    		<?php if ( has_post_thumbnail() ) { the_post_thumbnail('rs-tmb'); } else {echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/images/broke-image.jpg" />'; }?>
@@ -75,7 +83,7 @@
 		    	</div>
 				<?php endwhile; endif; wp_reset_query(); ?> 
 			</div>
-
+		</div>
 		<?php get_sidebar('instagram') ?>			
 		<?php get_sidebar('twitter') ?>
 	</div>
