5bb0b7f5303194f723c80305f6b3b590d7f1dfa5
Daniel Osorio Merchan <didanito@gmail.com>
2015-04-09 01:16:04 -0500

    paginador single

--- wp-content/themes/manawynwood/single-post.php
+++ wp-content/themes/manawynwood/single-post.php
@@ -5,7 +5,7 @@
 			<?php while ( have_posts() ) : the_post(); ?>
 				
 				<div class="large-12 columns post-image">
-					<?php the_post_thumbnail('rs-tmb'); ?>
+					<?php the_post_thumbnail(); ?>
 				</div>
 				<div class="large-12 columns">
 					<h1><?php the_title(); ?></h1>
@@ -18,16 +18,34 @@
 				</div>
 			<?php endwhile; ?>
 		</div>
-		<div class="large-3 columns">
+		<div class="large-3 columns sidebar">
 			<?php get_sidebar('main') ?>
 			<div class="large-12 columns pagination">
-	            <div class="prev large-6 columns">
-	                <span class="large-12 columns">prev</span>
-	                <?php previous_post('%', '', 'yes'); ?> 
-	            </div>
-	            <div class="next large-6 columns">
-	                <span class="large-12 columns">next</span>
-	                <?php next_post('%', '', 'yes'); ?>
+				<div id="cooler-nav" class="navigation">
+		            <?php $prevPost = get_previous_post(true);
+						if($prevPost) {?>
+						<div class="nav-box prev large-6 columns" style="float:left;">
+			                <?php $prevthumbnail = get_the_post_thumbnail($prevPost->ID, array(100,100) );}?>
+			                <div class="large-6 columns" style="padding-right: 5px">
+			                	<?php previous_post_link('%link',"$prevthumbnail", TRUE); ?>
+			                </div>
+			                <div class="large-6 columns">
+			                	<?php previous_post_link('%link',"%title", TRUE); ?>
+			                </div>
+						</div>
+
+		            <?php $nextPost = get_next_post(true);
+						if($nextPost) { ?>
+		            <div class="nav-box next large-6 columns" style="float:right;">
+						<?php $nextthumbnail = get_the_post_thumbnail($nextPost->ID, array(100,100) ); } ?>
+						<div class="large-6 columns">
+		                	<?php next_post_link('%link',"%title", TRUE); ?>
+		                </div>
+						<div class="large-6 columns" style="padding-left: 5px">
+							<?php next_post_link('%link',"$nextthumbnail ", TRUE); ?>
+		                </div>
+
+	                </div>
 	            </div>
 	        </div>
 		</div>
@@ -55,4 +73,10 @@
 	    	<a class="link-to-all" href="#">All Press</a>
 		</section>
 	</div>
+	<script type="text/javascript">
+		jQuery(document).ready(function($){
+			var content_heihgt = $('.container-post').height();
+			$('.sidebar').css('height', content_heihgt)
+		});
+	</script>
 <?php get_footer(); ?>
